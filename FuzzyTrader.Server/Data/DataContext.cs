7b49ac051ffbaffd2db29ba7ff4ed5e11ce8cc31
Igor Sodré <csodreigor@gmail.com>
2021-04-25 18:28:20 -0300

    Add migration for Trade assets table

--- FuzzyTrader.Server/Data/DataContext.cs
+++ FuzzyTrader.Server/Data/DataContext.cs
@@ -1,5 +1,4 @@
 using FuzzyTrader.Server.Data.DbEntities;
-using FuzzyTrader.Server.Extensions.Database;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,11 +11,12 @@ namespace FuzzyTrader.Server.Data
         public DbSet<Wallet> Wallets { get; set; }
         public DbSet<Investment> Investments { get; set; }
         public DbSet<CryptoCoin> CryptoCoins { get; set; }
+        public DbSet<TradeAsset> TradeAssets { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            builder.Seed();
-        }
+        // protected override void OnModelCreating(ModelBuilder builder)
+        // {
+        //     base.OnModelCreating(builder);
+        //     builder.Seed();
+        // }
     }
 }
---
+++ FuzzyTrader.Server/Data/Migrations/20210425212700_addTradeAssetTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FuzzyTrader.Server.Data.Migrations
+{
+    public partial class addTradeAssetTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TradeAssets",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Open = table.Column<decimal>(type: "numeric", nullable: true),
+                    High = table.Column<decimal>(type: "numeric", nullable: true),
+                    Low = table.Column<decimal>(type: "numeric", nullable: true),
+                    Close = table.Column<decimal>(type: "numeric", nullable: true),
+                    Volume = table.Column<decimal>(type: "numeric", nullable: true),
+                    AdjHigh = table.Column<decimal>(type: "numeric", nullable: true),
+                    AdjLow = table.Column<decimal>(type: "numeric", nullable: true),
+                    AdjClose = table.Column<decimal>(type: "numeric", nullable: true),
+                    AdjOpen = table.Column<decimal>(type: "numeric", nullable: true),
+                    AdjVolume = table.Column<decimal>(type: "numeric", nullable: true),
+                    SplitFactor = table.Column<decimal>(type: "numeric", nullable: true),
+                    Symbol = table.Column<string>(type: "text", nullable: true),
+                    Exchange = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TradeAssets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TradeAssets");
+        }
+    }
+}
