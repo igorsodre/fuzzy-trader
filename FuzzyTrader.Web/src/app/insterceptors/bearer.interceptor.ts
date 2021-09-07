4a7f31745897cfe8f684c0e3c137a7965887709f
Igor Sodré <csodreigor@gmail.com>
2021-09-07 15:24:33 -0300

    fix http services not rejecting promises

--- FuzzyTrader.Web/src/app/insterceptors/bearer.interceptor.ts
+++ FuzzyTrader.Web/src/app/insterceptors/bearer.interceptor.ts
@@ -1,8 +1,7 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { EMPTY, from, Observable, throwError } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { TokenService } from '../services/token.service';
-import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class BearerInterceptor implements HttpInterceptor {
@@ -13,25 +12,13 @@ export class BearerInterceptor implements HttpInterceptor {
   }
 
   private async handle(request: HttpRequest<unknown>, next: HttpHandler) {
-    console.log('Entering interceptor BearerInterceptor');
     let accessToken = '';
     try {
       accessToken = await this.tokenService.getAccessToken();
-    } catch {
-      console.log('Catch bearer after trying to get access token');
-    }
+    } catch {}
 
     request.headers.append('Authorization', `Bearer ${accessToken}`);
 
-    return next
-      .handle(request)
-      .pipe(
-        catchError((error) => {
-          console.log('catchError on bearer interceptor');
-          console.log(error);
-          return EMPTY;
-        }),
-      )
-      .toPromise();
+    return next.handle(request).toPromise();
   }
 }
